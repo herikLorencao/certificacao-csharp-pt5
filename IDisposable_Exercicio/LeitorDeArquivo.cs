819a678ffb6e78011cb5c6e0489ac43848804e4d
Herik dos Santos Lorenção <heriksantoslorencao@gmail.com>
2022-02-21 18:12:12 -0300

    adicionando padrão IDisposable

---
+++ IDisposable_Exercicio/LeitorDeArquivo.cs
@@ -0,0 +1,24 @@
+﻿namespace IDisposable_Antes;
+
+public class LeitorDeArquivo : IDisposable
+{
+    public string Arquivo { get; set; }
+
+    public LeitorDeArquivo(string arquivo)
+    {
+        Arquivo = arquivo;
+        Console.WriteLine("Abrindo arquivo " + arquivo);
+    }
+
+    public string LerProximaLinha()
+    {
+        Console.WriteLine("Lendo linha ...");
+        throw new IOException();
+        return "Linha do arquivo";
+    }
+
+    public void Dispose()
+    {
+        Console.WriteLine("Fechando arquivo");
+    }
+}
---
+++ IDisposable_Exercicio/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace IDisposable_Antes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CarregarContas();
+        }
+
+        public static void CarregarContas()
+        {
+            using (LeitorDeArquivo leitor = new LeitorDeArquivo("contas.txt"))
+            {
+                leitor.LerProximaLinha();
+                leitor.LerProximaLinha();
+                leitor.LerProximaLinha();
+            }
+        }
+    }
+}
